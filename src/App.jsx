0f8a0aaa9c30aedd19258c76527e8b756bf9a776
DARSHAN KUMAR N <darshankumar372003@gmail.com>
2024-03-25 16:54:25 +0530

    initial basic version done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
 
+import Postlist from "./components/Postlist";
+import "./App.css"
+import { useState } from "react";
 function App() {
- return 
+
+    const [selectedtab,setselectedtab]=useState("Home");
+ return  <div className="app-container">
+    <Sidebar selectedtab={selectedtab} setselectedtab={setselectedtab}></Sidebar>
+    <div className="content">
+    <Header></Header>
+    {selectedtab === "Home" ?<Postlist></Postlist>:<CreatePost></CreatePost>}
+    
+    
+<Footer></Footer>
+    </div>
+ 
+ </div>
+ 
+
 }
 
 export default App
---
+++ src/components/Postlist.jsx
@@ -0,0 +1,10 @@
+import Post from "./Post";
+const Postlist=() =>{
+return <>
+<Post></Post>
+<Post></Post>
+<Post></Post>
+<Post></Post>
+<Post></Post></>
+}
+export default Postlist;
